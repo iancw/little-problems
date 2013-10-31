66ea72cb331a954ccfc49f3608acff83919a6f8b
Ian Will <icwill@gmail.com>
2013-10-30 22:11:14 -0400

    Tested delete

--- my_hash.py
+++ my_hash.py
@@ -10,12 +10,20 @@ class my_hash:
           self.values = []
 
         def find_idx(self, key):
-          for i in range(0, len(self.keys)):
+          """
+          Find index of key, runs in O(n) time
+          """
+          for i in range(0, self.size):
             if self.keys[i] == key:
               return i
           return None
 
         def insert(self, key, value):
+          """
+          Insert key, value pair, runs in O(n) time 
+          because of find_idx call to search for existing
+          entries.
+          """
           existing = self.find_idx(key)
           if existing is None:
             self.keys.append(key)
@@ -25,12 +33,18 @@ class my_hash:
           self.size += 1
 
         def get(self, key):
+          """
+          Retrieve value for key, O(n)
+          """
           idx = self.find_idx(key)
           if idx is None:
             return None
           return self.values[idx]
 
         def delete(self, key):
+          """
+          Remove key, value pair if they exist, runs in O(n) time
+          """
           idx = self.find_idx(key)
           if idx is None:
             return None
--- test_hash.py
+++ test_hash.py
@@ -14,6 +14,14 @@ class TestHeap(unittest.TestCase):
     h.insert("hash", 2)
     self.assertEqual(2, h.get("hash"))
 
+  def test_delete(self):
+    h = my_hash()
+    h.insert("hashyhash", 1)
+    self.assertEquals(1, h.get("hashyhash"))
+    h.delete("hashyhash")
+    self.assertEquals(None, h.get("hashyhash"))
+
+
 if __name__ == '__main__':
   unittest.main()
 
